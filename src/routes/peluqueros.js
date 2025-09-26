d27731ccdd29c4694dd295dcc9d08a3e9e7c45b0
Luca <lucasimodev01@gmail.com>
2025-09-26 08:58:54 -0300

    ..

--- src/routes/peluqueros.js
+++ src/routes/peluqueros.js
@@ -3,14 +3,18 @@ import Peluquero from "../models/Peluqueros.js";
 
 const router = express.Router();
 
-// Obtener todos los peluqueros activos desde Atlas
 router.get("/", async (req, res) => {
   try {
     const peluqueros = await Peluquero.find({ activo: true });
+    console.log("PELQUEROS DESDE DB:", peluqueros); // 👈 Para debug en Render
+    res.set("Cache-Control", "no-store");
     res.json(peluqueros);
   } catch (error) {
+    console.error("ERROR AL TRAER PELUQUEROS:", error);
     res.status(500).json({ error: error.message });
   }
 });
+
+
 
 export default router;
