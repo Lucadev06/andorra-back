9090daad7dc22ec9f2fd06473547032034bdc4d4
Luca <lucasimodev01@gmail.com>
2025-10-11 09:57:56 -0300

    ..

--- src/controllers/peluqueroController.js
+++ src/controllers/peluqueroController.js
@@ -65,11 +65,12 @@ export const addDiaLibre = async (req, res) => {
       return res.status(404).json({ message: "Peluquero not found" });
     }
 
-    peluquero.diasLibres.push(new Date(fecha));
+    peluquero.diasLibres.push(new Date(fecha + 'T00:00:00'));
     await peluquero.save();
 
     res.status(200).json(peluquero);
   } catch (err) {
+    console.error(err);
     res.status(500).json({ message: err.message });
   }
 };
